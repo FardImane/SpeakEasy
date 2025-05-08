0d4e7b3a6a123449cdd9157973434b94065210f1
FardImane <imane.fard@etu.uae.ac.ma>
2025-05-08 23:25:12 +0200

    add changes

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -15,7 +15,7 @@ const Newsletter = () => {
     setStatus('sending');
 
     try {
-      const response = await fetch('/api/newsletter/subscribe', {
+      const response = await fetch('/server/server.js', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
